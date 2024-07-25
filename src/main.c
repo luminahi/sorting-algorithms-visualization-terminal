be71fa3127d4070bedcc3e2804bc08edee2a52f9
Alex Lima <delimaferreiraalex@gmail.com>
2024-07-25 00:14:24 -0300

    ref: add options

--- src/main.c
+++ src/main.c
@@ -3,6 +3,7 @@
 #include <time.h>
 #include "sorting.h"
 
+#define BAR_SYMBOL '.'
 #define MAX_BAR_SIZE 48
 #define MAX_ARRAY_SIZE 32
 
@@ -21,23 +22,51 @@ void update_screen(char* bar, int* array, int array_size) {
   for (int i = 0; i < array_size; i++) {
     bar[array[i]] = '\0';
     printf("%s\n", bar);
-    bar[array[i]] = '#';
+    bar[array[i]] = BAR_SYMBOL;
   }
 
   wait(0, 10000000L);
 }
 
-int main(int argc, char* argv[]) {
-  char bar[MAX_BAR_SIZE];
-  for (int i = 0; i < MAX_BAR_SIZE; i++) bar[i] = '#';
-
-  srand(time(NULL));
-  int arr[MAX_ARRAY_SIZE];
+void shuffle(int* array) {
   for (int i = 0; i < MAX_ARRAY_SIZE; i++) {
-    arr[i] = (rand() % (MAX_BAR_SIZE - 1)) + 1;
+    array[i] = (rand() % (MAX_BAR_SIZE - 1)) + 1;
   }
+}
 
-  insertion_sort(arr, MAX_ARRAY_SIZE, bar, &update_screen);
+int main(int argc, char* argv[]) {
+  srand(time(NULL));
+
+  char bar[MAX_BAR_SIZE];
+  for (int i = 0; i < MAX_BAR_SIZE; i++) bar[i] = BAR_SYMBOL;
+
+  int array[MAX_ARRAY_SIZE];
+  shuffle(array);
+
+  char option[2];
+  while (1) {
+    printf("1: Bubble Sort\n2: Selection Sort\n3: Insertion Sort\n0: Quit\n\n");
+    scanf("%2s", option);
+
+    switch (*option) {
+    case '1':
+      bubble_sort(array, MAX_ARRAY_SIZE, bar, &update_screen);
+      break;
+    case '2':
+      selection_sort(array, MAX_ARRAY_SIZE, bar, &update_screen);
+      break;
+    case '3':
+      insertion_sort(array, MAX_ARRAY_SIZE, bar, &update_screen);
+      break;
+    case '0':
+      exit(EXIT_SUCCESS);
+    default:
+      break;
+    }
+
+    shuffle(array);
+  }
+  // insertion_sort(arr, MAX_ARRAY_SIZE, bar, &update_screen);
   // selection_sort(arr, bar, &update_screen);
   // bubble_sort(arr, bar, &update_screen);
   return 0;
